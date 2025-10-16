7f0040699419b1f6c0abcf320233ee5a182f0ea7
Moudycode963 <moudycode963@gmail.com>
2025-10-16 09:20:40 +0200

    fehler Behebung

--- view/index.php
+++ view/index.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 
-<!--<img src='--><?php //= DOMAIN_NAME . "/public/assets/images/logo.png" ?><!--' alt="meinlogo" name="" width="500">-->
+<img src='<?= DOMAIN_NAME . "/assets/images/logo.png" ?>' alt="meinlogo" name="" width="100">
 <h1>Willkomen</h1>
 
 
